9ca6854b21a3836f0789e6c0da48239a910a74a2
Matthew Turk <matthewturk@gmail.com>
2019-10-11 16:40:29 -0500

    Adding yt-tools imports

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,31 @@ import {
   JupyterFrontEnd, JupyterFrontEndPlugin
 } from '@jupyterlab/application';
 
+import {
+    ICommandPalette, MainAreaWidget
+} from '@jupyterlab/apputils';
+
+import {
+    Widget
+} from '@phosphor/widgets';
+
+import {
+    Colormap, ColormapCollection, FixedResolutionBuffer, RGBAValue,
+    VariableMesh
+} from '@data-exp-lab/yt-tools';
+
+class VariableMeshExplorerWidget extends Widget
+{
+    constructor() {
+        super();
+        this.addClass("yt-vm-widget");
+        this.canvas = document.createElement("canvas");
+        this.node.appendChild(this.canvas);
+    }
+
+    readonly canvas: HTMLCanvasElement;
+}
+
 
 /**
  * Initialization data for the jupyterlab_yt extension.
